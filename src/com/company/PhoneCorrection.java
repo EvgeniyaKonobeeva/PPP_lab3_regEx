1bfd014021c79281cceec4f498968ce28d752296
Evgeniya Konobeeva <zknbeeva@mail.ru>
2016-04-12 01:44:38 +0300

    составила регулярку для номера

--- src/com/company/PhoneCorrection.java
+++ src/com/company/PhoneCorrection.java
@@ -11,10 +11,15 @@ public class PhoneCorrection {
     private InputStream in;
     private OutputStream out;
     String gr1 = "(\\s*(\\(*|\\-*)\\s*)"; // gr1,2
-    String gr2 = "((\\d{1,3}\\s*){1,3})"; //gr3, 4
+    String gr2 = "((\\d\\s*\\d\\s*\\d){1})"; //gr3, 4
     String gr3 = "(\\s*(\\-*|\\)*)\\s*)"; //gr 5, 6
-    String gr4 = "((\\d{1,2}\\s*){1,2})";//gr 7,8
+    String gr4 = "((\\d\\s*\\d){1})";//gr 7,8
     private String regEx = "\\+*" + gr1 +"\\d*(\\1)" + gr2 + gr3 + "(\\1)(\\3)(\\5)(\\1)"+gr4 + "(\\5)(\\1)(\\7)";
+
+    String reg = "(\\+*((\\(*|\\-*)\\s*)\\d?(\\s*(\\(*|\\-*)\\s*)" +
+            "((\\d\\s*\\d\\s*\\d){1})(\\s*(\\)*|\\-*)\\s*)(\\s*(\\(*|\\-*)\\s*)" +
+            "((\\d\\s*\\d\\s*\\d){1})(\\s*(\\)*|\\-*)\\s*)(\\s*(\\(*|\\-*)\\s*)" +
+            "((\\d\\s*\\d){1})(\\s*(\\-*|\\)*)\\s*)(\\s*(\\(*|\\-*)\\s*)((\\d\\s*\\d){1}))";
     public PhoneCorrection(String _fileNameIn, String _fileNameOut){
         this.fileNameIn = _fileNameIn;
         this.fileNameOut = _fileNameOut;
