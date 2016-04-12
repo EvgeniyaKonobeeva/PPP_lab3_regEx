05df1839d099a0f4b6d0280e62853946705848d2
Evgeniya Konobeeva <zknbeeva@mail.ru>
2016-04-12 18:37:30 +0300

    сделана функция считывания текста

--- src/com/company/PhoneCorrection.java
+++ src/com/company/PhoneCorrection.java
@@ -1,6 +1,8 @@
 package com.company;
 
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Евгения on 11.04.2016.
@@ -10,11 +12,13 @@ public class PhoneCorrection {
     private String fileNameOut;
     private InputStream in;
     private OutputStream out;
+    private BufferedReader reader;
+    private BufferedWriter writer;
     String gr1 = "(\\s*(\\(*|\\-*)\\s*)"; // gr1,2
     String gr2 = "((\\d\\s*\\d\\s*\\d){1})"; //gr3, 4
     String gr3 = "(\\s*(\\-*|\\)*)\\s*)"; //gr 5, 6
     String gr4 = "((\\d\\s*\\d){1})";//gr 7,8
-    private String regEx = "\\+*" + gr1 +"\\d*(\\1)" + gr2 + gr3 + "(\\1)(\\3)(\\5)(\\1)"+gr4 + "(\\5)(\\1)(\\7)";
+    private String regEx = "\\+*" + gr1 +"\\d*"+gr1 + gr2 + gr3 + gr1+ gr2 +gr3 + gr1 +gr4 + gr3+ gr1 + gr4;
 
     String reg = "(\\+*((\\(*|\\-*)\\s*)\\d?(\\s*(\\(*|\\-*)\\s*)" +
             "((\\d\\s*\\d\\s*\\d){1})(\\s*(\\)*|\\-*)\\s*)(\\s*(\\(*|\\-*)\\s*)" +
@@ -26,12 +30,28 @@ public class PhoneCorrection {
         try {
             in = new BufferedInputStream(new FileInputStream(fileNameIn));
             out = new BufferedOutputStream(new FileOutputStream(fileNameOut));
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileNameIn)));
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileNameIn)));
+
+
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
     }
-    public void CorrectNumbers(int CountryCode){
+    public void CorrectNumbers(int CountryCode)throws IOException{
+        Pattern pat =Pattern.compile(regEx);
+        Matcher mat;
+        String before, number, after, whole;
+        while(reader.read() != -1){
+            whole = reader.readLine();
+            mat = pat.matcher(whole);
+            while(mat.find()){
+                before = whole.substring(0,mat.start());
+                number = whole.substring(mat.start(), mat.end());
 
+                after = whole.substring(mat.end());
+            }
+        }
     }
 
 }
